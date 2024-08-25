e063711b00777f247c4df8b8880d11646cd09ca9
maniael <manirathinam@ulektz.in>
2024-08-25 16:02:50 +0530

    First Commit

---
+++ main.py
@@ -0,0 +1,5 @@
+import torch
+print(torch.__version__)
+print(torch.cuda.is_available())  # If you installed the GPU version
+
+# git remote add origin https://mani1430:mani14360@github.com/mani1430/translater.git
